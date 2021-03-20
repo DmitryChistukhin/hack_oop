c1ca5f6a0a095224d56b28d79593ce59678c11ca
Дмитрий Чистухин <M0rbid@mail.ru>
2021-03-20 19:43:04 +0300

    FirstResult

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ class Thing:
         self.name = random.choice(self.Names)
         self.attack = float(random.randint(0, 10))
         self.defence = random.randint(1, 10) * 0.01
-        self.health = float(random.randint(0, 100))
+        self.health = random.randint(0, 100)
 
     def __str__(self):
         return f'Вы получили {self.name} с параметрами: атака = {self.attack}, защита = {self.defence}, здоровье = {self.health}'
@@ -31,57 +31,61 @@ class Person:
 
     def __init__(self):
         self.name = random.choice(self.Names)
-        self.attack = float(random.randint(0, 30))
+        self.attack = random.randint(0, 30)
         self.defence = random.randint(1, 10) * 0.01
-        self.health = float(random.randint(0, 500))
-        self.setThings = []
+        self.health = random.randint(0, 15)
+        self.things = []
 
     def setThings(self):
         n = random.randint(1, 4)
         for i in range (1, n+1):
             newthing = random.choice(Things_list)
-            self.setThings.append(newthing(i))
+            self.things.append(newthing(i))
 
     def updated_attack(self):
         updated_attack = self.attack + sum(
-            Thing.attack for Thing in self.setThings)
+            Thing.attack for Thing in self.things)
         return updated_attack
     
     def updated_defence(self):
         updated_defence = self.defence + sum(
-            Thing.defence for Thing in self.setThings)
+            Thing.defence for Thing in self.things)
         return updated_defence
     
     def updated_health(self):
         updated_health = self.health + sum(
-            Thing.health for Thing in self.setThings)
+            Thing.health for Thing in self.things)
         return updated_health
         
-
     def __str__(self):
-        return f'Создан персонаж {self.name} с параметрами: атака = {self.attack}, защита = {self.defence}, здоровье = {self.health}'
+        return f'Создан персонаж {self.name} с параметрами: атака = {self.updated_attack}, защита = {self.defence}, здоровье = {self.health}'
+
+p1 = Person()
+print(p1)
 
 
 class Paladin(Person):
     def __init__(self):
         self.name = random.choice(self.Names)
-        self.attack = float(random.randint(0, 30))
+        self.attack = random.randint(0, 30)
         self.defence = random.randint(1, 10) * 0.01 * 2
-        self.health = float(random.randint(0, 500)) * 2 
+        self.health = random.randint(0, 15) * 2 
+        self.things = []
 
     def __str__(self):
-        return f'Создан паладин {self.name} с параметрами: атака = {self.attack}, защита = {self.defence}, здоровье = {self.health}'
+        return f'Паладин {self.name} с параметрами: атака = {self.attack}, защита = {self.defence}, здоровье = {self.health}'
 
 
 class Warrior(Person):
     def __init__(self):
         self.name = random.choice(self.Names)
-        self.attack = float(random.randint(0, 30)) * 2
+        self.attack = random.randint(0, 30) * 2
         self.defence = random.randint(1, 10) * 0.01
-        self.health = float(random.randint(0, 500))
+        self.health = random.randint(0, 15)
+        self.things = []
 
     def __str__(self):
-        return f'Создан воин {self.name} с параметрами: атака = {self.attack}, защита = {self.defence}, здоровье = {self.health}'
+        return f'Воин {self.name} с параметрами: атака = {self.attack}, защита = {self.defence}, здоровье = {self.health}'
 
 n=10
 Paladin_list = []
@@ -99,23 +103,17 @@ for i in range(1, n+1):
     Fighter = random.choice(Classes_list)
     Fighters_list.append(Fighter)
 
-     
-
 while len(Fighters_list) > 1:
     attacker = random.choice(Fighters_list)
     defender = random.choice(Fighters_list)
-    dh = defender.updated_health
-    aa = attacker.updated_attack
-    dd = defender.updated_defence
-
-    hp_left = dh - (aa - aa*dd)
+    dh = defender.updated_health()
+    aa = attacker.updated_attack()
+    dd = defender.updated_defence()
+    damage = aa - aa*dd
+    hp_left = dh - damage
+    print(f'{attacker.name} наносит удар по {defender.name} на {damage} урона')
     if hp_left <= 0:
         Fighters_list.remove(defender)
 else:
     winner = random.choice(Fighters_list)
-    print (f'победитель: {winner}')
-
-
-
-
-    
+    print (f'победитель: {winner}')   
